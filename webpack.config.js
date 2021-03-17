681292a2cb10854b10300f71d8649e3899d85246
seif1125 <57404468+seif1125@users.noreply.github.com>
2021-03-17 11:53:58 +0200

    configure webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,18 @@
 const path=require('path')
-module.exports={
-    entry:"./src/index.js",
-    output:{
-       filename:"build.js",
-       path:path.resolve(__dirname,"build")
-    },
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "build.js",
+    path: path.resolve(__dirname, "build"),
+  },
 
-    module:{
-        rules:[{
-            test:'/\.css$/',
-            use:["style-loader",
-                "css-loader"
-            ]
-        }]
-    }
-}
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
